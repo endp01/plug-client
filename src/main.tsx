9b0e23bdcbf08a09d579bbf827cc4b42b2913b49
nftchance <chance@utc24.io>
2023-11-08 16:31:15 -0600

    fix: small progress

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,30 @@
 import React from 'react'
-
-import App from './App.tsx'
-import './index.css'
-import ReactDOM from 'react-dom/client'
-
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-import { WagmiConfig } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { WagmiConfig } from 'wagmi'
 
 import {
-    createTRPCProxyClient,
-    createWSClient,
-    httpBatchLink,
-    splitLink,
-    wsLink
+	createTRPCProxyClient,
+	createWSClient,
+	httpBatchLink,
+	splitLink,
+	wsLink
 } from '@trpc/client'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 
-import { AppRouter } from '../../server/api'
-
-import Permission from './pages/Permission.tsx'
+import { AppRouter } from '../../server/src/api'
+import App from './App.tsx'
+import './index.css'
 import Intents from './pages/Intents.tsx'
+import Permission from './pages/Permission.tsx'
+import ReactDOM from 'react-dom/client'
+import { mainnet } from 'wagmi/chains'
 
 const projectId = '91222ec4c7d553d05264028fb3276d8c'
 
 const metadata = {
-    name: 'Emporium',
-    description: 'Emporium',
-    url: 'https://emporium.app',
+	name: 'Emporium',
+	description: 'Emporium',
+	url: 'https://emporium.app'
 }
 
 const chains = [mainnet]
@@ -37,34 +33,34 @@ const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
 createWeb3Modal({ wagmiConfig, projectId, chains })
 
 const wsClient = createWSClient({
-    url: 'ws://localhost:3000/trpc'
+	url: 'ws://localhost:3000/trpc'
 })
 const client = createTRPCProxyClient<AppRouter>({
-    links: [
-        splitLink({
-            condition: op => {
-                return op.type === 'subscription'
-            },
-            true: wsLink({
-                client: wsClient
-            }),
-            false: httpBatchLink({
-                url: 'http://localhost:3000/trpc'
-            })
-        })
-    ]
+	links: [
+		splitLink({
+			condition: op => {
+				return op.type === 'subscription'
+			},
+			true: wsLink({
+				client: wsClient
+			}),
+			false: httpBatchLink({
+				url: 'http://localhost:3000/trpc'
+			})
+		})
+	]
 })
 
 const router = createBrowserRouter([
-    { path: '/', element: <App /> },
-    { path: '/permission', element: <Permission client={client} /> },
-    { path: '/intents', element: <Intents client={client} /> }
+	{ path: '/', element: <App /> },
+	{ path: '/permission', element: <Permission client={client} /> },
+	{ path: '/intents', element: <Intents client={client} /> }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <WagmiConfig config={wagmiConfig}>
-            <RouterProvider router={router} />
-        </WagmiConfig>
-    </React.StrictMode>
+	<React.StrictMode>
+		<WagmiConfig config={wagmiConfig}>
+			<RouterProvider router={router} />
+		</WagmiConfig>
+	</React.StrictMode>
 )
--- src/pages/Permission.tsx
+++ src/pages/Permission.tsx
@@ -1,86 +1,93 @@
+import { constants } from '@nftchance/emporium-types'
+
+import { useEffect, useState } from 'react'
+import { useAccount, useSignTypedData } from 'wagmi'
+
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
+import { DEFAULT_PERMISSION } from '@/lib/constants'
 import { createTRPCProxyClient } from '@trpc/client'
+import { useWeb3Modal } from '@web3modal/wagmi/react'
 
-import { constants } from '@nftchance/emporium-types'
-import { useWeb3Modal } from "@web3modal/wagmi/react"
-import { useAccount, useSignTypedData } from "wagmi"
-
-import { AppRouter } from "../../../server/api"
-import { DEFAULT_PERMISSION } from "@/lib/constants"
-import { useEffect, useState } from 'react'
+import { AppRouter } from '../../../server/src/api'
 
 export default function Permission<
-    T extends ReturnType<typeof createTRPCProxyClient<AppRouter>>,
-    P extends {
-        client: T
-    }
+	P extends {
+		client: ReturnType<typeof createTRPCProxyClient<AppRouter>>
+	}
 >({ client }: P) {
-    const { open } = useWeb3Modal()
-    const { address } = useAccount()
+	const { open } = useWeb3Modal()
+	const { address } = useAccount()
 
-    const [query, setQuery] = useState(JSON.stringify(DEFAULT_PERMISSION, null, 4));
+	const [query, setQuery] = useState(
+		JSON.stringify(DEFAULT_PERMISSION, null, 4)
+	)
 
-    const { data, isError, isLoading, isSuccess, reset, signTypedData } = useSignTypedData({
-        domain: DEFAULT_PERMISSION.domain,
-        message: DEFAULT_PERMISSION.message.permission,
-        primaryType: 'Permission',
-        types: constants.types,
-        onSuccess: (signature) => {
-            const stateQuery = JSON.parse(query)
-            stateQuery.message.signature = signature
-            setQuery(JSON.stringify(stateQuery, null, 4))
-        }
-    })
+	const { data, isError, isLoading, isSuccess, reset, signTypedData } =
+		useSignTypedData({
+			domain: DEFAULT_PERMISSION.domain,
+			message: DEFAULT_PERMISSION.message.permission,
+			primaryType: 'Permission',
+			types: constants.types,
+			onSuccess: signature => {
+				const stateQuery = JSON.parse(query)
+				stateQuery.message.signature = signature
+				setQuery(JSON.stringify(stateQuery, null, 4))
+			}
+		})
 
-    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-        e.stopPropagation()
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+		e.stopPropagation()
 
-        const json = JSON.parse(query)
-        await client.permissions.create.mutate(json)
+		const json = JSON.parse(query)
+		await client.permissions.create.mutate(json)
 
-        reset()
-    }
+		reset()
+	}
 
-    useEffect(() => { 
-        await client.permissions.onCreate(data => { 
-            console.log('Permission created: ', data)
-        })
-    }, [])
+	useEffect(() => {
+		// * Open the onCreate susbcription.
+		client.permissions.onCreate.subscribe(undefined, {
+			onData: id => {
+				console.log('Permission created:', id)
+			}
+		})
+	}, [client])
 
-    return (
-        <>
-            <Button onClick={() => open({ view: 'Networks' })}>Networks</Button>
-            <Button onClick={() => open()}>Connect</Button>
+	return (
+		<>
+			<Button onClick={() => open({ view: 'Networks' })}>Networks</Button>
+			<Button onClick={() => open()}>Connect</Button>
 
-            <p>Connected Address: {address ? address : 'Not Connected'}</p>
+			<p>Connected Address: {address ? address : 'Not Connected'}</p>
 
-            <form
-                onSubmit={handleSubmit}
-                className="space-y-8"
-            >
-                <label htmlFor="name">
-                    Permission
-                    <Textarea
-                        name="permission"
-                        value={query}
-                        className="h-[50vh]"
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                </label>
+			<form onSubmit={handleSubmit} className="space-y-8">
+				<label htmlFor="name">
+					Permission
+					<Textarea
+						name="permission"
+						value={query}
+						className="h-[50vh]"
+						onChange={e => setQuery(e.target.value)}
+					/>
+				</label>
 
-                <Button type="button" onClick={() => signTypedData({})}
-                    disabled={isLoading || !address || !query}
-                >{isLoading ? 'Signing...' : 'Sign'}</Button>
+				<Button
+					type="button"
+					onClick={() => signTypedData({})}
+					disabled={isLoading || !address || !query}
+				>
+					{isLoading ? 'Signing...' : 'Sign'}
+				</Button>
 
-                <Button type="submit"
-                    disabled={!isSuccess}
-                >Submit</Button>
+				<Button type="submit" disabled={!isSuccess}>
+					Submit
+				</Button>
 
-                {isSuccess && <p>Signature: {data}</p>}
-                {isError && <p>Error signing message.</p>}
-            </form>
-        </>
-    )
+				{isSuccess && <p>Signature: {data}</p>}
+				{isError && <p>Error signing message.</p>}
+			</form>
+		</>
+	)
 }
